23462fdb13a894d3a7f914729a55e54be549ad8a
Daijon Bourne <Daijonbourne1005@gmail.com>
2024-04-17 22:33:35 -0700

    Updated the app.js folder

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Character from './components/Character';
 
 function App() {
+  var image1  = 'https://images.unsplash.com/photo-1586671267731-da2cf3ceeb80?q=80&w=989&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
   return (
     <div className="App">
+    <div className='Title'>
+      <h1>Who is Stronger</h1>
+    </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Character></Character>
+      <Character></Character>
       </header>
     </div>
   );
